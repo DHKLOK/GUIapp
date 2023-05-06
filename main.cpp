752ad70554e168f95d876e59abd7d1a9fb5760ee
DHKLOK <DHKLOK@DESKTOP-7JS3G20>
2023-05-06 18:06:18 -0400

    Stable image

--- main.cpp
+++ main.cpp
@@ -1,7 +1,150 @@
 #include <windows.h>
 #include "Resource.h"
 
+INT_PTR CALLBACK BananaDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static HBITMAP hBitmap = NULL;
+   
+    switch (message)
+    {
+    case WM_INITDIALOG:
+    {
+        if (hBitmap == NULL)
+        {
+            hBitmap = (HBITMAP)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_BANANA_IMAGE), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
+        }
+        SendDlgItemMessage(hDlg, IDC_BANANA_PICTURE, STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hBitmap);
+        return TRUE;
+    }
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return TRUE;
+        }
+        break;
+    case WM_CLOSE:
+    {
+        if (hBitmap)
+        {
+            DeleteObject(hBitmap);
+            hBitmap = NULL;
+        }
+        EndDialog(hDlg, 0);
+        return TRUE;
+    }
+    }
 
+
+    return FALSE;
+}
+
+INT_PTR CALLBACK LOWRESPICDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static HBITMAP hBitmap = NULL;
+
+    switch (message)
+    {
+    case WM_INITDIALOG:
+    {
+        if (hBitmap == NULL)
+        {
+            hBitmap = (HBITMAP)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_LOWRESPIC_IMAGE), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
+        }
+        SendDlgItemMessage(hDlg, IDC_LOWRESPIC_PICTURE, STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hBitmap);
+        return TRUE;
+    }
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return TRUE;
+        }
+        break;
+    case WM_SIZE:
+    {
+        int width = LOWORD(lParam);
+        int height = HIWORD(lParam);
+
+        // Resize the image control
+        HWND hImageControl = GetDlgItem(hDlg, IDC_LOWRESPIC_PICTURE);
+        SetWindowPos(hImageControl, NULL, (width - 230) / 2, 10, width - 20, height - 20, SWP_NOZORDER | SWP_NOACTIVATE);
+
+        // Load the original image
+        HBITMAP hOriginalBitmap = (HBITMAP)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_LOWRESPIC_IMAGE), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
+
+        // Create a new resized image maintaining aspect ratio
+        HBITMAP hResizedBitmap = NULL;
+        {
+            HDC hdcScreen = GetDC(NULL);
+            HDC hdcOriginal = CreateCompatibleDC(hdcScreen);
+            HDC hdcResized = CreateCompatibleDC(hdcScreen);
+
+            HBITMAP hOldOriginalBitmap = (HBITMAP)SelectObject(hdcOriginal, hOriginalBitmap);
+
+            // Calculate new width and height while maintaining aspect ratio
+            BITMAP originalBitmapInfo;
+            GetObject(hOriginalBitmap, sizeof(BITMAP), &originalBitmapInfo);
+            int originalWidth = originalBitmapInfo.bmWidth;
+            int originalHeight = originalBitmapInfo.bmHeight;
+            float aspectRatio = (float)originalWidth / originalHeight;
+
+            int newWidth = (int)((height - 20) * aspectRatio);
+            int newHeight = height - 20;
+
+            // Create a compatible bitmap with the new dimensions
+            hResizedBitmap = CreateCompatibleBitmap(hdcScreen, newWidth, newHeight);
+            HBITMAP hOldResizedBitmap = (HBITMAP)SelectObject(hdcResized, hResizedBitmap);
+
+            // Stretch the original image to fit the new dimensions
+            SetStretchBltMode(hdcResized, HALFTONE);
+            StretchBlt(hdcResized, 0, 0, newWidth, newHeight, hdcOriginal, 0, 0, originalWidth, originalHeight, SRCCOPY);
+
+            SelectObject(hdcOriginal, hOldOriginalBitmap);
+            SelectObject(hdcResized, hOldResizedBitmap);
+
+            DeleteDC(hdcOriginal);
+            DeleteDC(hdcResized);
+            ReleaseDC(NULL, hdcScreen);
+        }
+
+        // Set the resized image to the image control
+        SendMessage(hImageControl, STM_SETIMAGE, IMAGE_BITMAP, (LPARAM)hResizedBitmap);
+
+        // Clean up
+        DeleteObject(hOriginalBitmap);
+        if (hBitmap)
+        {
+            DeleteObject(hBitmap);
+            hBitmap = NULL;
+        }
+        hBitmap = hResizedBitmap;
+
+        return TRUE;
+    }
+
+
+
+
+
+
+
+    
+    case WM_CLOSE:
+    {
+        if (hBitmap)
+        {
+            DeleteObject(hBitmap);
+            hBitmap = NULL;
+        }
+        EndDialog(hDlg, 0);
+        return TRUE;
+    }
+    }
+
+
+    return FALSE;
+}
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
     case WM_DESTROY:
@@ -29,6 +172,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             MessageBox(hWnd, L"Paul's Project\nVersion 1.0", L"About", MB_OK | MB_ICONINFORMATION);
             break;
 
+        /*case ID_MENU_BANANA:
+            // Handle "Banana" command
+            MessageBox(hWnd, L"You clicked the Banana Item!", L"Banana", MB_OK | MB_ICONINFORMATION);
+            break;*/
+        case ID_MENU_BANANA:
+            // Handle "Banana" command
+            DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_BANANA_DIALOG), hWnd, BananaDialogProc);
+            break;
+        case ID_MENU_LOWRESPIC:
+            // Handle "Hentai" command
+            DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_LOWRESPIC_DIALOG), hWnd, LOWRESPICDialogProc);
+            break;
+        
         default:
             return DefWindowProc(hWnd, message, wParam, lParam);
         }
